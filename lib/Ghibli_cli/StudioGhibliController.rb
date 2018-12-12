e144b4cdd848a12ea60b61135d20d64233cf72c3
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-12 09:03:53 +0000

    finished working controller

--- lib/Ghibli_cli/StudioGhibliController.rb
+++ lib/Ghibli_cli/StudioGhibliController.rb
@@ -7,17 +7,17 @@ class StudioGhibliController
     puts "Welcome to the Studio Ghibli Movie Generator!"
     puts "You can generate a random Studio Ghibli film by its producer or Rotten Tomatoes score."
     puts "To list all film producers, enter 'list producers'."
-    puts "To list all Rotten Tomatoes scores, enter 'list scores'."
+    puts "To list all Rotten Tomatoes scores, enter 'list RT scores'."
     puts "To generate a random film by producer, enter the name of the producer."
     puts "To generate a random film by Rotten Tomatoes score, enter the score."
     puts "To quit, type 'exit'."
-    puts "Enter a list to get started."
+    puts "To get started, enter the list you wish to see."
     input = gets.chomp
       case input
       when 'list producers'
         list_producers
-        when 'list scores'
-        list_scores
+        when 'list RT scores'
+        list_rt_scores
       when 'producer name'
         generate_from_producer
       when 'score'
@@ -26,24 +26,27 @@ class StudioGhibliController
     end
   end
   
-  
-  
   def list_producers
     puts <<-DOC
-    Enter a film year from the list to generate a Studio Ghibli film:
-      1984, 1986, 1988, 1989, 1991, 1992, 1993, 1994,
-      1995, 1997, 1999, 2000, 2001, 2002, 2004, 2006,
-      2008, 2009, 2010, 2011, 2013, 2014, 2018
+    Enter a producer from the list:
+      Isao Takahata, Toru Hara, Hayao Miyazaki, Toshio Suzuki, Nozomu Takahashi, Yoshiaki Nishimura, Seiichiro Ujiie
     DOC
   end
   
-  def list_scores
+  def list_rt_scores
+    puts <<-DOC
+    Enter a Rotten Tomatoe score from the list:
+      41, 75, 78, 83, 87, 89, 90
+      92, 93, 94, 95, 97, 100
+    DOC
   end
 
   def generate_from_producer
+    puts film_info
   end
 
   def generate_from_score
+    puts film_info
   end
 
 end
