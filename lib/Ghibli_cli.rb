9d95dd22e4e975ef561c398ab6a99f24e7526d18
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-12 08:11:18 +0000

    first commit

---
+++ lib/Ghibli_cli.rb
@@ -0,0 +1,6 @@
+require "Ghibli_cli/version"
+
+module GhibliCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Ghibli_cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GhibliCli do
+  it "has a version number" do
+    expect(GhibliCli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
