32129246c8479c383267b12101488317fc2f6c43
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-26 23:36:39 +0000

    modified scraper

--- lib/Ghibli_cli/api_scraper.rb
+++ lib/Ghibli_cli/api_scraper.rb
@@ -1,9 +1,36 @@
+require 'Nokogiri'
 require 'open-uri'
 require_relative './lib/Ghibli_cli/film.rb'
 
 class Scraper
-  def self.get_film
-    data = open("https://ghibliapi.herokuapp.com/films")
-    JSON.parse(data)
+  
+attr_accessor :get_films
+  
+  def initialize
+  Nokogiri::HTML(open("https://ghibliapi.herokuapp.com/films"))
+  @get_films ||=Nokogiri::HTML(html)
   end
+  
+  def get_film_title
+    get_films.text("title")
+  end
+  
+  def get_film_release_date
+    get_films.text("release_date")
+  end
+  
+  def get_film_produce
+    get_films.text("producer")
+  end
+  
+  def get_film_rt_score
+    get_films.text("rt_score")
+  end
+  
+  def get_film_description
+    get_films.text("description")
+  end
+  
+  def make_film
+    self.
 end
--- lib/Ghibli_cli/studioghiblicontroller.rb
+++ lib/Ghibli_cli/studioghiblicontroller.rb
@@ -1,29 +1,36 @@
 #CLI controller
 class StudioGhibliController
   
-  attr_reader :producer
+  attr_reader :film , :producer
   
   def initialize
-    @producer = producer
-    importer_obj = Producer.new(name)
-    importer_obj.import
+    @film = film
   end
   
   def call
     input = ''
     while input != 'exit'
     puts "Welcome to the Studio Ghibli Movie Generator!"
-    puts "You can generate a random Studio Ghibli film by its producer."
+    puts "To list all Studio Ghibli films, enter 'list films'."
+    puts "You can generate a random Studio Ghibli film by typing its producer."
     puts "To list all film producers, enter 'list producers'."
     puts "To generate a random film by producer, enter the name of the producer."
+    puts "To quit, type 'exit'."
+    puts "To get started, enter 'list films' or 'list producers'."
     input = gets.chomp
       case input
+      when 'list films'
+        list_films
       when 'list producers'
         list_producers
       when 'producer name'
         generate_from_producer
       end
     end
+  end
+  
+  def list_films
+   Film.all
   end
   
   def list_producers
