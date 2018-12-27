ca8b587a140cb4eccdb97ffc225978514aff206d
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-27 00:37:55 +0000

    edited scraper

--- lib/Ghibli_cli/api_scraper.rb
+++ lib/Ghibli_cli/api_scraper.rb
@@ -1,36 +1,9 @@
-require 'Nokogiri'
 require 'open-uri'
-require_relative './lib/Ghibli_cli/film.rb'
 
 class Scraper
   
-attr_accessor :get_films
-  
-  def initialize
-  Nokogiri::HTML(open("https://ghibliapi.herokuapp.com/films"))
-  @get_films ||=Nokogiri::HTML(html)
+def self.get_films
+    data = open("https://ghibliapi.herokuapp.com/films")
+    JSON.parse(data)
   end
-  
-  def get_film_title
-    get_films.text("title")
-  end
-  
-  def get_film_release_date
-    get_films.text("release_date")
-  end
-  
-  def get_film_produce
-    get_films.text("producer")
-  end
-  
-  def get_film_rt_score
-    get_films.text("rt_score")
-  end
-  
-  def get_film_description
-    get_films.text("description")
-  end
-  
-  def make_film
-    self.
 end
--- lib/Ghibli_cli/film.rb
+++ lib/Ghibli_cli/film.rb
@@ -2,12 +2,7 @@ class Film
  
   attr_accessor :title, :release_date, :producer, :rt_score, :description
  
-  def initialize
-    @title = title
-    @release_date = release_date
-    @producer = producer
-    @rt_score = rt_score
-    @description = description
-    
+  def initialize(film_hash)
+    film_hash.each do |text|
   end
 end
