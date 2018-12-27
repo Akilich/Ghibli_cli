084f06f7e88c23f0e94eaf00bae392dffba23357
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-27 09:08:42 +0000

    updated scrapper

--- lib/api_scraper.rb
+++ lib/api_scraper.rb
@@ -4,8 +4,13 @@ require 'json'
 
 class Scraper
   def self.get_films
-    data = open('https://ghibliapi.herokuapp.com/films').read
+    data= open('https://ghibliapi.herokuapp.com/films').read
    JSON.parse(data)
+  
+  puts data('title').text
+  puts data('release_date').text
+  puts data('producer').text
+  puts data('rt_score').text
+  puts data('description').text
   end
 end
-
