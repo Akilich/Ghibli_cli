09811262f161067cee79784a4fa5b008ab5ca75f
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-27 17:41:21 +0000

    updated scraper

--- lib/api_scraper.rb
+++ lib/api_scraper.rb
@@ -5,9 +5,25 @@ require 'json'
 #require 'httparty'
 
 class Scraper
-  def self.get_films
-    data = open("https://ghibliapi.herokuapp.com/films")
-    JSON.parse(data)
+    def self.get_films
+      #film_information = open("https://ghibliapi.herokuapp.com/films").read
+      #JSON.parse(film_information)
+      Nokogiri::HTML(open("https://ghibliapi.herokuapp.com/films"))
+      film_information = Nokogiri::HTML#(html)
+      films ={ }
+      
+    
+    self.get_films.each do |film|
+    title = film("title").text
+    projects[title.to_sym] = {
+      :release_date => film("release_date").attribute("src").value,
+      :producer => film("producer").text,
+      :rt_score => film("rt_score").text,
+      :descr=> film("description").text
+    }
+    end
+  
+    films
   end
 end
 
