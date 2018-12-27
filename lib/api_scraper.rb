ad656ab376da514396b98ac670b51fa193c43f7b
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-27 09:38:25 +0000

    working parse

--- lib/api_scraper.rb
+++ lib/api_scraper.rb
@@ -1,13 +1,14 @@
-require 'open-uri'
-require 'net/http'
-require 'json'
+#require 'open-uri'
+#require 'net/http'
+#require 'json'
+require 'httparty'
 
 class Scraper
   def self.get_films
-    data = open('https://ghibliapi.herokuapp.com/films').read
-   JSON.parse(data)
-  
- film_information = ("title, release_date, producer, rt_score, description")
+    #data = open('https://ghibliapi.herokuapp.com/films').read
+   #JSON.parse(data)
+  response = HTTParty.get('https://ghibliapi.herokuapp.com/films')
+  standard = JSON.parse(response.to_json)
   end
 end
 
